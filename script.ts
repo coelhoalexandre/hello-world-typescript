c14d3d7d686dd6d668f4dfdc13693b1c406d3e03
Alexandre Coelho <116454874+coelhoalexandre@users.noreply.github.com>
2024-06-23 10:46:10 -0300

    boas vindas com erro de tipagem

--- script.ts
+++ script.ts
@@ -1,16 +1,21 @@
+interface IUsuario {
+  nome: string;
+  sobrenome: string;
+  idade: number;
+}
 function chamarOlaMundo() {
   return "Hello World";
 }
 
-function chamarBoasVindas(nome, sobrenome) {
+function chamarBoasVindas(nome: string, sobrenome: string) {
   return "Boas-vindas ao TypeScript, " + nome + " " + sobrenome + "!";
 }
 
-const usuario = {
+const usuario: IUsuario = {
   nome: "Alexandre",
   sobrenome: "Coelho",
 };
 
 alert(chamarOlaMundo());
 
-alert(chamarBoasVindas(usuario.nome, usuario.sobrenome));
+alert(chamarBoasVindas(usuario.nome, usuario.sobrenome, usuario.idade));
