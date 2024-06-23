820bf3e18db2bac7c33b16866d4a659025400fa7
Alexandre Coelho <116454874+coelhoalexandre@users.noreply.github.com>
2024-06-23 10:56:50 -0300

    boas-vindas-javascript

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function chamarOlaMundo() {
     return "Hello World";
 }
+function chamarBoasVindas(nome, sobrenome) {
+    return "Boas-vindas ao TypeScript, " + nome + " " + sobrenome + "!";
+}
+var usuario = {
+    nome: "Alexandre",
+    sobrenome: "Coelho",
+};
 alert(chamarOlaMundo());
+alert(chamarBoasVindas(usuario.nome, usuario.sobrenome));
